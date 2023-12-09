4b82679df940dacb0a9f5c3bc2ed2c9bc3094ab0
Chirag Singh Chhonkar <Chhonkarchirag886@gmail.com>
2023-12-09 17:18:50 +0530

    commit

--- Yummyresto/settings.py
+++ Yummyresto/settings.py
@@ -135,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  # Use the appropriate port for your SMTP provider
 EMAIL_USE_TLS = True  # Use TLS for security
-EMAIL_HOST_USER = 'example@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'rchirag553@gmail.com'
+EMAIL_HOST_PASSWORD = 'zggvpmsmioigolri'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
