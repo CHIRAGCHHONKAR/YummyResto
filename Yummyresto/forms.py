25c743b325ab4ee294db4a5ed8b391fc567fd1cd
Chirag Singh Chhonkar <Chhonkarchirag886@gmail.com>
2024-07-15 14:48:40 +0530

    Commit

--- Yummyresto/forms.py
+++ Yummyresto/forms.py
@@ -3,18 +3,18 @@ from django import forms
 
 class ReservationForm(forms.Form):
     TIME_OF_DAY_CHOICES = (
-        ('', 'Time'),
+        ('', ''),
         ('morning', 'Morning'),
         ('afternoon', 'Afternoon'),
         ('evening', 'Evening'),
     )
-    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'p-2 form-control','placeholder': 'Your Name'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'p-2 form-control','placeholder': 'Your Email'}))
-    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'p-2 form-control','placeholder': 'Your Phone'}))
-    date = forms.DateField(widget=forms.DateInput(attrs={'class': 'p-2 form-control','placeholder': 'Date','type': 'date',}))
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'p-2 form-control',}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'p-2 form-control',}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'p-2 form-control',}))
+    date = forms.DateField(widget=forms.DateInput(attrs={'class': 'p-2 form-control','type': 'date',}))
     time = forms.ChoiceField(choices=TIME_OF_DAY_CHOICES, widget=forms.Select(attrs={'class': ' p-2 form-control'}))
-    people = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'p-2 form-control','placeholder': '# of people'}))
-    comments = forms.CharField(required=False,widget=forms.Textarea(attrs={'class': 'p-2 form-control', 'rows': 5, 'cols': 101,'placeholder': 'Message'}))
+    people = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'p-2 form-control'}))
+    comments = forms.CharField(required=False,widget=forms.Textarea(attrs={'class': 'p-2 form-control', 'rows': 5, 'cols': 101,}))
     
     
 
